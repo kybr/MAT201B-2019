aacb4d3b926d8043ac470ee09f2f26dc55c541a8
Karl Yerkes <karl.yerkes@gmail.com>
2019-01-24 15:22:56 -0800

    starter code ready

--- example/color_spaces.cpp
+++ example/color_spaces.cpp
@@ -1,35 +1,71 @@
 #include "al/core.hpp"
 #include "al/util/al_Image.hpp"  // gets us Image
 using namespace al;
+using namespace std;
 
 struct MyApp : App {
-  Texture texture;
+  Mesh mesh{Mesh::POINTS};
+  vector<Vec3f> position[4];  // an array of 4 vectors of Vec3f
+  // position[0] is the original "image"-coordinates
+  // position[1] is the RGB
+  // position[2] is the HSV cylinder
+  // position[3] is your choice
+
   void onCreate() override {
-    // load an image file
     Image image;
     const char* fileName = "../color_spaces.jpg";
-    if (image.load(fileName)) {
-      printf("Read image from %s\n", fileName);
+    if (!image.load(fileName)) {
+      exit(1);
     }
-    image.sendToTexture(texture);
+
+    Image::RGBAPix<uint8_t> pixel;
+    for (int row = 0; row < image.height(); row++) {
+      for (int column = 0; column < image.width(); column++) {
+        image.read(pixel, column, row);  // read pixel data
+        mesh.vertex(column / image.width(), row / image.width(), 0);
+        position[0].push_back(
+            Vec3f(column / image.width(), row / image.width(), 0));
+        mesh.color(pixel.r / 256.0, pixel.g / 256.0, pixel.b / 256.0);
+      }
+    }
+
+    // you initialize...
+    // position[1]
+    // position[2]
+    // position[3]
   }
 
+  int target, last;  // use like position[target] or position[last]
+
+  double timer = 0;
   void onAnimate(double dt) override {
+    // dt is in seconds
+    timer += dt;
+    // timer is now the amount of time (in seconds) since the program started
+
+    // http://paulbourke.net/miscellaneous/interpolation/
+
     // animate the positions of the mesh vertices
+    auto& vertexList = mesh.vertices();
+    // for each vertex in the list above
+    // interpolate (linear) between 'last' and 'target'
   }
 
   void onDraw(Graphics& g) override {
     g.clear(0.25);
-    //
-    // draw the pixel mesh
-
-    g.translate(0, 0, -5);
-    g.quad(texture, -1, -1, 2, 2);
+    g.meshColor();  // alternative to g.color(1, 0, 0);
+    g.draw(mesh);
   }
 
   void onKeyDown(const Keyboard& k) override {
     if (k.key() == '1') {
-      printf("got a 1 key press\n");
+      // start process of animating to position[0]
+    } else if (k.key() == '2') {
+      // start process of animating to position[1]
+    } else if (k.key() == '3') {
+      // start process of animating to position[2]
+    } else if (k.key() == '4') {
+      // start process of animating to position[3]
     }
   }
 };
--- example/vector-and-vec.cpp
+++ example/vector-and-vec.cpp
@@ -5,8 +5,34 @@ using namespace al;
 using namespace std;
 
 int main() {
-  vector<float> floatList;
+  Vec3f v;  // defined by allolib and used a LOT
+  Vec3f up(0, 1, 0);
+
+  v.x = 1;
+  v.y += 0.1;
+  v.y += 0.1;
+  v.y += 0.1;
+
+  up.dot(v);                 // the dot product from linear algebra
+  Vec3f out = cross(up, v);  // cross product from linear algebra
+
+  Mesh mesh;
+  mesh.primitive(Mesh::TRIANGLES);
+  mesh.vertex(0, 0, 0);  // vertex 0
+  mesh.vertex(0, 1, 0);  // vertex 1
+  mesh.vertex(1, 0, 0);  // vertex 2
+  auto& verts = mesh.vertices();
+  cout << verts[1].y << endl;
+
+  // std::vector is different than Vec3f
+  // std::vector is defined by the c++ standard library
+  vector<float> floatList;  // these are arrays, but better
   floatList.push_back(3.3);
-  Vec3f v;
-  v.x = 0.1;
+
+  for (int i = 0; i < 10; ++i) floatList.push_back(rnd::uniform(1.0));
+
+  for (auto f : floatList)  // a "range" for loop
+    cout << f << endl;
+
+  cout << "the size is " << floatList.size() << endl;
 }
