92b04e170fae8f912e3d41bc00fed7139a08deb9
Karl Yerkes <karl.yerkes@gmail.com>
2019-01-31 18:27:28 -0800

    better mass implementation

--- example/mass-spring-two-dimension.cpp
+++ example/mass-spring-two-dimension.cpp
@@ -9,26 +9,26 @@ using namespace std;
 
 // 3 masses, 3 springs, in a triangle
 
-void ellipse(Graphics& g, float x, float y, float w, float h) {
+void circle(Graphics& g, Vec2f v, float r) {
   Mesh m{Mesh::TRIANGLE_FAN};
-  m.vertex(x, y);
+  m.vertex(v);
   for (int i = 0; i < 101; ++i)
-    m.vertex(x + w * sin(M_2PI * i / 100), y + h * cos(M_2PI * i / 100));
+    m.vertex(v.x + r * sin(M_2PI * i / 100), v.y + r * cos(M_2PI * i / 100));
   g.color(1);
   g.draw(m);
 }
 
-void line(Graphics& g, float x0, float y0, float x1, float y1) {
+void line(Graphics& g, Vec2f a, Vec2f b) {
   Mesh m{Mesh::LINES};
-  m.vertex(x0, y0);
-  m.vertex(x1, y1);
+  m.vertex(a);
+  m.vertex(b);
   g.color(1);
   g.draw(m);
 }
 
-struct Mass {
+struct Mass : Vec2f {
+  Vec2f velocity;
   float mass;
-  Vec2f position, velocity;
 };
 
 struct Spring {
@@ -45,9 +45,17 @@ struct MyApp : App {
   vector<Spring> spring;
 
   void onCreate() override {
-    mass.push_back({0.11, {0, 0}, {0, 0}});
-    mass.push_back({0.19, {1, 0}, {0, 0}});
-    mass.push_back({0.27, {0, 1}, {0, 0}});
+    mass.resize(3, Mass{});
+    mass[0].x = 1;
+    mass[0].mass = 0.11;
+
+    mass[1].y = 1;
+    mass[1].mass = 0.19;
+
+    mass[2].x = 1;
+    mass[2].y = 1;
+    mass[2].mass = 0.27;
+
     spring.push_back({mass[0], mass[1], 1, 200});
     spring.push_back({mass[1], mass[2], 1, 300});
     spring.push_back({mass[2], mass[0], 1, 200});
@@ -56,28 +64,26 @@ struct MyApp : App {
   void onAnimate(double dt) override {
     // caculate velocity change from forces
     for (Spring& s : spring) {
-      Vec2f a2b = s.b.position - s.a.position;
+      Vec2f a2b = s.b - s.a;
       float forceMagnitude =  // Fs = -kx (Hooke's Law)
           (a2b.mag() - s.restLength) * s.stiffness;
       Vec2f unitVector = a2b / a2b.mag();  // now length 1 (unit) vector
       Vec2f force = unitVector * forceMagnitude;
+
+      // equal and opposite forces
       s.a.velocity += force;
       s.b.velocity -= force;
     }
 
-    // drag
+    // add drag
     for (Mass& m : mass) {
       m.velocity += -m.velocity * dragFactor;
     }
 
     // integrate velocity
-
     for (Mass& m : mass) {
-      m.position += m.velocity * timeStep;
+      m += m.velocity * timeStep;
     }
-
-    // mass[0].position.print();
-    // cout << endl;
   }
 
   void onDraw(Graphics& g) override {
@@ -89,14 +95,10 @@ struct MyApp : App {
     g.scale(0.5);  // zoom out
 
     for (Mass& m : mass) {
-      ellipse(g, m.position.x, m.position.y, m.mass, m.mass);
+      circle(g, m, m.mass);
     }
-    // for (Type name : container) { /* body */ }
-    // for (Type& name : container) { /* body */ }
-    // for (auto& name : container) { /* body */ }
-    // for (auto& name : vector<??>) { /* body */ }
     for (Spring& s : spring) {
-      line(g, s.a.position.x, s.a.position.y, s.b.position.x, s.b.position.y);
+      line(g, s.a, s.b);
     }
   }
 
