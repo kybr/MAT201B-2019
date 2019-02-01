ad5cd55d79f9a5b650a7d45024312156f21171d1
Karl Yerkes <karl.yerkes@gmail.com>
2019-02-01 07:11:40 -0800

    commented spring example

--- example/mass-spring-two-dimension.cpp
+++ example/mass-spring-two-dimension.cpp
@@ -4,20 +4,18 @@ using namespace al;
 #include <vector>
 using namespace std;
 
-// make this 2d
-// make multiple masses and springs
-
-// 3 masses, 3 springs, in a triangle
-
-void circle(Graphics& g, Vec2f v, float r) {
+// draw a circle, given a 2D point and a radius
+void circle(Graphics& g, Vec2f center, float radius) {
   Mesh m{Mesh::TRIANGLE_FAN};
-  m.vertex(v);
+  m.vertex(center);
   for (int i = 0; i < 101; ++i)
-    m.vertex(v.x + r * sin(M_2PI * i / 100), v.y + r * cos(M_2PI * i / 100));
+    m.vertex(center.x + radius * sin(M_2PI * i / 100),
+             center.y + radius * cos(M_2PI * i / 100));
   g.color(1);
   g.draw(m);
 }
 
+// draw a line, given two points, a and b
 void line(Graphics& g, Vec2f a, Vec2f b) {
   Mesh m{Mesh::LINES};
   m.vertex(a);
@@ -26,34 +24,45 @@ void line(Graphics& g, Vec2f a, Vec2f b) {
   g.draw(m);
 }
 
+// Earlier, I defined Mass like this:
+//
+// struct Mass {
+//   Vec2f position;
+//   Vec2f velocity;
+//   float mass;
+// };
+//
+// I changed the definition of Mass to "inherit" from Vec2f so that we can treat
+// it as its own position. Below, we say that Mass *is* a Vec2f which represents
+// its position. As well, it has a Vec2f for velocity.
+//
 struct Mass : Vec2f {
   Vec2f velocity;
   float mass;
 };
 
 struct Spring {
-  Mass& a;
+  Mass& a;  // a reference to a Mass
   Mass& b;
-  float restLength, stiffness;
+  float restLength;
+  float stiffness;
 };
 
 struct MyApp : App {
-  double timeStep = 0.001;   // simulation
-  double dragFactor = 0.05;  // environment, atmosphere
+  // simulation parameters
+  double timeStep = 0.001;
+  double dragFactor = 0.05;
 
   vector<Mass> mass;
   vector<Spring> spring;
 
   void onCreate() override {
     mass.resize(3, Mass{});
-    mass[0].x = 1;
+    mass[0].set(0, 0);
     mass[0].mass = 0.11;
-
-    mass[1].y = 1;
+    mass[1].set(1, 0);
     mass[1].mass = 0.19;
-
-    mass[2].x = 1;
-    mass[2].y = 1;
+    mass[2].set(1, 1);
     mass[2].mass = 0.27;
 
     spring.push_back({mass[0], mass[1], 1, 200});
@@ -62,12 +71,20 @@ struct MyApp : App {
   }
 
   void onAnimate(double dt) override {
-    // caculate velocity change from forces
+    // For each Spring...
     for (Spring& s : spring) {
+      // Make a vector that points from a to b.
       Vec2f a2b = s.b - s.a;
-      float forceMagnitude =  // Fs = -kx (Hooke's Law)
-          (a2b.mag() - s.restLength) * s.stiffness;
-      Vec2f unitVector = a2b / a2b.mag();  // now length 1 (unit) vector
+
+      // Use Hooke's Law (Fs = -kx) to calculate the amount of force this
+      // Spring will exert on each Mass. Know that .mag() computes the length (a
+      // scalar) of a vector.
+      float forceMagnitude = (a2b.mag() - s.restLength) * s.stiffness;
+
+      // create a "unit vector" (a vector with length 1.0) that points in the
+      // direction of the Spring.
+      Vec2f unitVector = a2b / a2b.mag();
+
       Vec2f force = unitVector * forceMagnitude;
 
       // equal and opposite forces
@@ -88,11 +105,16 @@ struct MyApp : App {
 
   void onDraw(Graphics& g) override {
     g.clear(0.25);
+
+    // Establish a coordinate system where the window goes from (-1, -1) on the
+    // bottom left corner and (1, 1) on the top right corner.
     g.camera(Viewpoint::IDENTITY);
 
-    g.scale((float)height() / width(), 1, 1);  // correct aspect ratio
+    // scale the x dimension to correct the aspect ratio
+    g.scale((float)height() / width(), 1, 1);
 
-    g.scale(0.5);  // zoom out
+    // scale *all* dimensions; this will "zoom out".
+    g.scale(0.5);
 
     for (Mass& m : mass) {
       circle(g, m, m.mass);
@@ -103,6 +125,7 @@ struct MyApp : App {
   }
 
   void onKeyDown(const Keyboard& k) override {
+    // Kick Mass 0 in a random direction when the space bar is pressed
     if (k.key() == ' ') {
       float angle = rnd::uniform(M_2PI);
       mass[0].velocity.x = 40 * sin(angle);
