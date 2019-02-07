447ce08b41370f90f0ed506a8728081e2e45207f
Karl Yerkes <karl.yerkes@gmail.com>
2019-02-07 13:29:00 -0800

    sound example

--- example/sound-spectral-transformation.cpp
+++ example/sound-spectral-transformation.cpp
@@ -1,8 +1,15 @@
 #include "al/core.hpp"
 using namespace al;
 
+#include "Gamma/DFT.h"  //STFT
 #include "Gamma/SamplePlayer.h"
 using namespace gam;
+
+const int WINDOW_SIZE = 2048;
+const int FFT_SIZE = 1 << 13;
+
+STFT stft =
+    STFT(WINDOW_SIZE, WINDOW_SIZE / 4, FFT_SIZE - WINDOW_SIZE, HANN, MAG_PHASE);
 
 struct MyApp : App {
   SamplePlayer<float, gam::ipl::Linear, phsInc::Loop> samplePlayer;
@@ -15,7 +22,13 @@ struct MyApp : App {
 
   void onSound(AudioIOData& io) override {
     while (io()) {
-      float f = samplePlayer();
+      float s = samplePlayer();
+      if (stft(s)) {
+        for (int i = 0; i < stft.numBins(); ++i) {
+          stft.bin(i)[1] = 0;  // zero phase
+        }
+      }
+      float f = stft();
       io.out(0) = f;
       io.out(1) = f;
     }
