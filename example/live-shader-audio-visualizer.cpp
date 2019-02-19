24c23ca35784ec6cc0f2921fb6ce4574d3e05c02
Karl Yerkes <karl.yerkes@gmail.com>
2019-02-19 14:23:26 -0800

    from class

--- example/live-shader-audio-visualizer.cpp
+++ example/live-shader-audio-visualizer.cpp
@@ -14,8 +14,7 @@ layout (location = 0) in vec3 vertexPosition;
 uniform mat4 al_ModelViewMatrix;
 uniform mat4 al_ProjectionMatrix;
 void main() {
-    // XXX missing one step
-  gl_Position = al_ModelViewMatrix * vec4(vertexPosition, 1.0);
+  gl_Position = al_ProjectionMatrix * al_ModelViewMatrix * vec4(vertexPosition, 1.0);
 }
 )";
 
@@ -33,15 +32,42 @@ string slurp(string fileName) {
 struct MyApp : App {
   gam::SamplePlayer<float, gam::ipl::Linear, gam::phsInc::Loop> samplePlayer;
   ShaderProgram shader;
+  Texture texture;
 
   void onCreate() override {
     samplePlayer.load("../sound/8.wav");
     samplePlayer.loop();
     Sync::master().spu(audioIO().fps());
 
-    string fragmentCode;  // comes from a file
-    // slurp: file -> string
+    string fragmentCode = slurp("../shader-fragment.glsl");
     shader.compile(vertexCode, fragmentCode);
+  }
+
+  void onDraw(Graphics& g) override {
+    //
+    g.clear(0.25);
+    g.camera(Viewpoint::IDENTITY);
+
+    g.shader(shader);
+    g.shader().uniform("time", time);
+    g.shader().uniform("window", Vec2f(width(), height()));
+    g.shader().uniform("mouse", Vec2f(mouseX, mouseY));
+    Mesh m{Mesh::TRIANGLE_STRIP};
+    m.vertex(1, 1);
+    m.vertex(1, -1);
+    m.vertex(-1, 1);
+    m.vertex(-1, -1);
+
+    // g.color(1); // XXX overrides shader
+    g.draw(m);
+
+    // g.quadViewport(texture);
+  }
+
+  int mouseX, mouseY;
+  void onMouseMove(const Mouse& m) {
+    mouseX = m.x();
+    mouseY = m.y();
   }
 
   void onSound(AudioIOData& io) override {
@@ -51,15 +77,13 @@ struct MyApp : App {
       io.out(1) = f;
     }
   }
+
+  double time = 0;
   void onAnimate(double dt) override {
+    time += dt;
     //
     // check if there is a "new" version of the fragment shader
     // if so, reload, recompile
-  }
-
-  void onDraw(Graphics& g) override {
-    //
-    g.clear(0.25);
   }
 };
 
